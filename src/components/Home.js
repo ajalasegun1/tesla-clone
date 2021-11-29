7129dc5efae26dd6d08f12b460793aa53c56a90e
ajalasegun1 <ajalasegun1@gmail.com>
2021-11-29 15:54:41 +0100

    Sections created

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,48 @@ import Section from "./Section";
 function Home() {
   return (
     <Container>
-      <Section />
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroundImg="model-s.jpg"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroundImg="model-y.jpg"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroundImg="model-3.jpg"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroundImg="model-x.jpg"
+      />
+      <Section
+        title="Lowest Cost Solar Panels in America"
+        description="Money-back guarantee"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+        backgroundImg="solar-panel.jpg"
+      />
+      <Section
+        title="Accessories"
+        description=""
+        leftBtnText="Custom Order"
+        // rightBtnText="Existing Inventory"
+        backgroundImg="accessories.jpg"
+      />
     </Container>
   );
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,8 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 
-function Section() {
-  return <Wrap>Section</Wrap>;
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
+  // console.log(backgroundImg);
+  return (
+    <Wrap bgImg={backgroundImg}>
+      <ItemText>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </ItemText>
+
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+        </ButtonGroup>
+
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
+    </Wrap>
+  );
 }
 
 export default Section;
@@ -10,5 +34,53 @@ export default Section;
 const Wrap = styled.div`
   width: 100vw;
   height: 100vh;
-  background: orange;
+  background-image: ${(props) => `url("/images/${props.bgImg}")`};
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
+const RightButton = styled(LeftButton)`
+  color: black;
+  opacity: 0.65;
+  background: white;
+`;
+
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+`;
+
+const Buttons = styled.div``;
